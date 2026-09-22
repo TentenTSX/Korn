5ad43b3430597fef3aa4dc0c617d5f5e626a33f6
Fleury Léo <fleuryleo@macbook-air-de-fleury5.home>
2026-09-22 14:41:25 +0200

    Create HomeStory component

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from "../../components/Hero/Hero";
 import HomeNews from "../../components/HomeNews/HomeNews";
+import HomeStory from "../../components/HomeStory/HomeStory";
 
 function Home() {
   return (
     <>
       <Hero />
       <HomeNews />
+      <HomeStory />
     </>
   );
 }
